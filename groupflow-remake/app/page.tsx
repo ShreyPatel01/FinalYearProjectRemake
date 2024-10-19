bc7a2beb8959658733ca3c60afe36e728fc30f70
ShreyPatel01 <100435186+ShreyPatel01@users.noreply.github.com>
2024-10-19 18:10:50 +0100

    implemented Auth0 for account creation and login

--- groupflow-remake/app/page.tsx
+++ groupflow-remake/app/page.tsx
@@ -1,15 +1,36 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { getSession } from "@auth0/nextjs-auth0";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession();
+  const user = session?.user;
+
+  if (user) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <p className="font-bold text-6xl">Welcome {user.name}</p>
+        <a href="/api/auth/logout">
+          <Button
+            size="lg"
+            variant="outline"
+            className="mt-4 hover:underline text-xl"
+          >
+            Logout
+          </Button>
+        </a>
+      </main>
+    );
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <p className='font-bold text-6xl'>
-        Welcome Page
-      </p>
-      <a href='/register'>
-        <Button size='lg' variant='outline' className='mt-4 hover:underline text-xl'>
-          Navigate to registration page
+      <p className="font-bold text-6xl">Welcome Page</p>
+      <a href="/api/auth/login">
+        <Button
+          size="lg"
+          variant="outline"
+          className="mt-4 hover:underline text-xl"
+        >
+          Login
         </Button>
       </a>
     </main>
